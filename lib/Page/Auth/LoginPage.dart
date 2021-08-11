fb47a5a06e9775911fa998529ebb354ae479a9ff
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-08-11 10:23:22 +0530

    color and padding added

--- lib/Page/Auth/LoginPage.dart
+++ lib/Page/Auth/LoginPage.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:fooddelivery/Utills/Universalvariable.dart';
 
 class LoginPage extends StatelessWidget {
   // const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: UniversalVariables.whiteColor,
+        padding: EdgeInsets.only(top: 20.0,left: 20.0,right: 20.0),
+      ),
+    );
   }
 }
