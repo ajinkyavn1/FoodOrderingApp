c0247edeaf6f3ab4e478758970d7c1297735ce60
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-08-12 22:55:40 +0530

    Bloc Created

---
+++ lib/blocs/RegisterPageBloc.dart
@@ -0,0 +1,41 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:fooddelivery/Services/auth_Methods.dart';
+
+class RegisterPageBloc with ChangeNotifier {
+
+  AuthMethods mAuthMethods = AuthMethods();
+
+  bool isRegisterPressed = false;
+
+  String validateEmail(String email) {
+    if (email.isEmpty && EmailValidator.validate(email)) {
+      return 'Please enter valid email';
+    }
+    return null;
+  }
+
+  String validatePassword(String password) {
+    if (password.isEmpty && password.length<6) {
+      return 'Password should atleast contain 6 character';
+    }
+    return null;
+  }
+
+  String validatePhone(String phone) {
+    if (phone.isEmpty && phone.length < 6) {
+      return 'Invalid PhoneNo';
+    }
+    return null;
+  }
+
+  Future<void> validateFormAndRegister(GlobalKey<FormState> formKey, String userName, String password, String phone) async {
+    isRegisterPressed = true;
+    notifyListeners();
+    if(formKey.currentState.validate()){
+      await mAuthMethods.SignUp(userName, password,phone);
+      isRegisterPressed = false;
+      notifyListeners();
+    }
+  }
+}
